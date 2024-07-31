c7cbce7a029bd880c4ba5d5ad6957b21ae0e9816
Doniyor1806 <mydonis23@gmail.com>
2024-07-31 14:33:52 -0400

    updated instance

---
+++ modules/terraform-ec2-instance/main.tf
@@ -0,0 +1,7 @@
+resource "aws_ec2_instance" "default" {
+name = var.instance_name
+instance_type = var.instance_type
+
+  
+}
+
---
+++ modules/terraform-ec2-instance/variables.tf
@@ -0,0 +1,14 @@
+variable "instance_type" {
+    description = "Type instance to start"
+    default = "t3.micro"
+    type = string
+  
+}
+
+variable "instance_name" {
+    description = "my_ec2_instance"
+    default = "my_instance"
+    type = string
+  
+}
+
