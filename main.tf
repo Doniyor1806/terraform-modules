24ad7715e816dcd39fd3ba9551fc7be18b230677
Doniyor1806 <mydonis23@gmail.com>
2024-08-03 23:48:05 -0400

    module main updated

--- main.tf
+++ main.tf
@@ -8,34 +8,36 @@ terraform {
   }
 }
 
+provider "aws" {} #Yangi qo'shildi
+
 module "my_web_instance" {
-    source = "./modules/terraform-ec2-instance/"
-    my_ami = var.first_ami
-    my_instance_type = var.first_instance_type
-    my_environment = var.first_environment
-    my_first_project = var.my_first_project 
-  
+  source           = "./modules/terraform-ec2-instance/"
+  my_ami           = var.first_ami
+  my_instance_type = var.first_instance_type
+  my_environment   = var.first_environment
+  my_first_project = var.my_first_project
+
 }
 
 variable "first_ami" {
-    description = "I need ami ID"
-    type = string
-    default = "ami-08c90f7460d6e8377" # my real AMI ID 
-  
+  description = "I need ami ID"
+  type        = string
+  default     = "ami-08c90f7460d6e8377" # my real AMI ID 
+
 }
 variable "first_instance_type" {
-    type = string
-    default = "t2.micro"
-   
- }
+  type    = string
+  default = "t2.micro"
+
+}
 
 variable "first_environment" {
-    description = "List of allowed environments"
-    type = list(string)
-    default = ["dev", "test", "prod"]
-   
- }
+  description = "List of allowed environments"
+  type        = list(string)
+  default     = ["dev", "test", "prod"]
+
+}
 variable "my_first_project" {
-  type = string
+  type    = string
   default = "the project"
 }
--- modules/terraform-ec2-instance/child_module.tf
+++ modules/terraform-ec2-instance/child_module.tf
@@ -1,19 +1,19 @@
 resource "aws_instance" "my_instance" {
-ami = var.my_ami
-instance_type = var.my_instance_type
+  ami           = var.my_ami
+  instance_type = var.my_instance_type
 
 
-tags = {
-  Name        = "web-instance"
-  Environment = var.my_environment
-  Project     = var.my_first_project
+  tags = {
+    Name        = "web-instance"
+    Environment = var.my_environment
+    Project     = var.my_first_project
 
-}
+  }
 }
 
 
 variable "my_ami" {
-  
+
 }
 variable "my_instance_type" {
 }
